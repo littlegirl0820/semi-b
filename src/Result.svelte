fd0d66a08708a44b9af438a7cf660848839e49c3
09B20046 <09B20046@localhost>
2022-12-20 14:02:56 +0900

    リザルト画面修正

--- src/Result.svelte
+++ src/Result.svelte
@@ -5,6 +5,10 @@
 				   "過激派キリスト教徒から仏教徒を減らすアルバイトを受けたから",
 				   "「お前、最近宗教色強くね?」何があった?"];
 	let last_answer = "パソコンのマウスが水晶でパソコンのロゴが何かの札";
+	let flg = false;
+	function buttonFlg(){
+		flg = true;
+	}
 	function backTitle(){
 
 	}
@@ -12,19 +16,25 @@
 
 <main>
     <h1>結果発表</h1>
-    <h2>最初のお題<br>『{first_answer}』</h2>
-	<div class="centering">
-		<details>
-			<summary>...</summary>
-				{#each middle_answers as answer}
-        			<li>{answer}</li>
-				{/each}
-		</details>
-	</div>
-    <h2>最後の回答<br>「{last_answer}」</h2>
-	<div class="centering">
-		<button on:click={backTitle}>タイトルに戻る</button>
-	</div>
+	{#if !flg}
+		<div class="centering">
+			<button on:click={buttonFlg}>結果オープン</button>
+		</div>
+	{:else}
+    	<h2>最初のお題<br>『{first_answer}』</h2>
+		<div class="centering">
+			<details>
+				<summary>...</summary>
+					{#each middle_answers as answer}
+        				<li>{answer}</li>
+					{/each}
+			</details>
+		</div>
+    	<h2>最後の回答<br>「{last_answer}」</h2>
+		<div class="centering">
+			<button on:click={backTitle}>タイトルに戻る</button>
+		</div>
+	{/if}
 </main>
 
 <style>
