8b6c21be851c02048251ed6988b9b4c4e1ed3527
Ryu-Okamoto <u514203b@ecs.osaka-u.ac.jp>
2022-12-23 14:34:14 +0900

    made template

--- index.js
+++ index.js
@@ -7,10 +7,24 @@ const io = new Server(server);
 
 app.use(express.static('public'));
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-});
-
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('join', (json) => {
+    // TODO: ユーザ名の重複、ゲーム中かどうかの検査
+    result = {}
+    socket.emit(result)
+  });
+
+  socket.on('members', (json) => {
+    // TODO: メンバーの管理
+    members = {}
+    socket.emit(members)
+  })
+
+  // ...
+})
