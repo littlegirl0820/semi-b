c64617ac63a2f3cc11d9f9a1929e10d0dcff62cf
watamario15 <watamario15@users.noreply.github.com>
2023-01-28 09:52:19 +0000

    deploy: 4073f63bcb4b4c32f0273c9a45f55e1d0a1fb61d

--- _app/immutable/chunks/2-4da9c254.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-be08b371.js";export{t as component};
---
+++ _app/immutable/chunks/2-4f278e69.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-ed36f671.js";export{t as component};
